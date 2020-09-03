ebc3ab91d9af74ddc9d01e8ccf2316f6f469011e
Pedro <pedro@Macmini-Pedro.local>
2020-09-03 17:57:00 -0300

    drag in drop retirado peridiocamente

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,20 +1,14 @@
 import React,{ useState,useEffect } from 'react';
 // Immer permite mudar estdados do useState e converter automaticamente para estados imutaveis
 // Essa biblioteca permite que seja possivel a utilização de metodos como push, slice, e etc.
-import immer from 'immer';
-
-import { loadLists } from '../../services/api';
 
 import { Container } from './styles';
 import List from '../List';
 // Puxa a api
-import ContextBoard from './context';
 import { getboard } from '../../server/api';
-const data = loadLists();
 
 export default function Board() {
 
-  const [lists,setLists] = useState(data);
   const [board,setBoard] = useState('');
 
   useEffect(()=>{
@@ -27,22 +21,21 @@ export default function Board() {
     setBoard(data);
   }
 
-  function move(fromList,toList,from,to) {
-    setLists(immer(lists,draft =>{
-      const dragged = draft[fromList].cards[from]
-      draft[fromList].cards.splice(from, 1);
-      draft[toList].cards.splice(to, 0, dragged);
+  // async function move(fromList,toList,from,to) {
+  //   await setLists(immer(lists,draft =>{
+  //     const dragged =  draft[fromList].steps[from]
+  //     draft[fromList].steps.splice(from, 1);
+  //     draft[toList].steps.splice(to, 0, dragged);
 
-    }))
-  }
+  //   }))
+  // }
   // getboard();
   return (
-    <ContextBoard.Provider value={{lists,move}}>
+
       <Container>
         {/* {lists.map((list,index) => <List key={list.title} index={index} data={list}/>)} */}
         {/* Optional chaning */}
         {board?.steps?.map((step,index) => <List key={step.id} index={index} data={step} />)}
       </Container>
-    </ContextBoard.Provider>
   )
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,64 +1,64 @@
 // Devido ao componente Container já possuir uma referência, é necessário a utilização do hook useRef
 import React,{useRef,useContext} from 'react'
 // Hook de drag in drop use drag faz a lógica de arastar e o use drop cria a funcionalidade adicionar o card em outra lista
-import { useDrag, useDrop } from 'react-dnd';
-import { Container,Label } from './styles';
+// import { useDrag, useDrop } from 'react-dnd';
+import { Container } from './styles';
 import { MdDelete } from 'react-icons/md';
-import ContextBoard from '../Board/context';
+// import ContextBoard from '../Board/context';
 export default function Card({ data,index,listIndex}) {
   const ref = useRef();
 
-  const { move } = useContext(ContextBoard);
-  // Essa parte de código é o que cria a lógica de drag in drop
-  const [ {isDragging }, dragRef] = useDrag({
-    item:{ type:'CARD', index,listIndex },
-    collect:monitor=>({
-      isDragging:monitor.isDragging(),
+  // const { move } = useContext(ContextBoard);
+  // // Essa parte de código é o que cria a lógica de drag in drop
+  // const [ {isDragging }, dragRef] = useDrag({
+  //   item:{ type:'CARD', index,listIndex },
+  //   collect:monitor=>({
+  //     isDragging:monitor.isDragging(),
 
-    })
-  })
+  //   })
+  // })
 
-  const [, dropRef] = useDrop({
-    accept:'CARD',
-    hover(item,monitor) {
-      const draggedListIndex = item.listIndex;
-      const targetListIndex = listIndex;
-      const draggedIndex = item.index;
-      const targetIndex = index;
+  // const [, dropRef] = useDrop({
+  //   accept:'CARD',
+  //   hover(item,monitor) {
+  //     const draggedListIndex = item.listIndex;
+  //     const targetListIndex = listIndex;
+  //     const draggedIndex = item.index;
+  //     const targetIndex = index;
 
-      if(draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
-        return;
-      }
+  //     if(draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
+  //       return;
+  //     }
 
-      const targetSize = ref.current.getBoundingClientRect();
-      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
-      const draggedOffset = monitor.getClientOffset();
-      const draggedTop = draggedOffset.y - targetSize.top;
+  //     const targetSize = ref.current.getBoundingClientRect();
+  //     const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+  //     const draggedOffset = monitor.getClientOffset();
+  //     const draggedTop = draggedOffset.y - targetSize.top;
 
-      // Essa condição não permite que um elemento que que está dragging e que não passou pelo elemento a ser trocado, mude de posição.
-      // nesse caso, ele só faz o calculo de altura após o item ultrapassar a metade do item que o item selecionado quer trocar de posição.
-      if(draggedIndex < targetIndex && draggedTop < targetCenter) {
-        return;
-      }
+  //     // Essa condição não permite que um elemento que que está dragging e que não passou pelo elemento a ser trocado, mude de posição.
+  //     // nesse caso, ele só faz o calculo de altura após o item ultrapassar a metade do item que o item selecionado quer trocar de posição.
+  //     if(draggedIndex < targetIndex && draggedTop < targetCenter) {
+  //       return;
+  //     }
 
-      // Essa verificação é ativada quando o item selecionado está abaixo do item a ser trocado
-      if(draggedIndex > targetIndex && draggedTop > targetCenter) {
-        return;
-      }
+  //     // Essa verificação é ativada quando o item selecionado está abaixo do item a ser trocado
+  //     if(draggedIndex > targetIndex && draggedTop > targetCenter) {
+  //       return;
+  //     }
 
-      move(draggedListIndex,targetListIndex,draggedIndex,targetIndex);
+  //     move(draggedListIndex,targetListIndex,draggedIndex,targetIndex);
 
-      item.index = targetIndex;
-      item.listIndex = targetListIndex;
+  //     item.index = targetIndex;
+  //     item.listIndex = targetListIndex;
 
 
-    }
-  })
-  // Criando a variavel useRef, eu consigo passar as duas refêrencias no componente
-  dragRef(dropRef(ref));
+  //   }
+  // })
+  // // Criando a variavel useRef, eu consigo passar as duas refêrencias no componente
+  // dragRef(dropRef(ref));
   return (
-    // o container recebe a referencia e o comportamento.
-    <Container ref={ref} isDragging={isDragging}>
+    // o container recebe a referencia e o comportamento. //isDragging=
+    <Container ref={ref}>
 
       <header>
         {/* {data.labels.map(label => <Label key={label} color={label} />)} */}
